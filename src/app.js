2390f78f037920b13c895698d84fb8bdcdc2f263
Roy Talyosef <roy@productionmap.com>
2017-12-03 10:22:58 +0200

    added exists funtion
    a method that checks if a path exists

--- src/app.js
+++ src/app.js
@@ -1,67 +1,78 @@
-var exec = require('child_process').exec;
-var q = require('q');
+let exec = require('child_process').exec;
+let fs = require("fs");
+let q = require('q');
 
-function executeCMD(action){
-    var deferred = q.defer();
-    var execString = action.method.actionString;
-    for (var i =0; i< action.method.params.length;i++){
-        var param = action.method.params[i].name;
+function executeCMD(action) {
+    let deferred = q.defer();
+    let execString = action.method.actionString;
+    for (let i = 0; i < action.method.params.length; i++) {
+        let param = action.method.params[i].name;
         if (action.params.hasOwnProperty(param)) {
             execString = execString.replace(param, action.params[param]);
         }
-        else{
+        else {
             execString = execString.replace(param, '');
         }
     }
     console.log(execString);
     exec(execString,
-        function(error, stdout, stderr){
+        function (error, stdout, stderr) {
 
-            if(error || stderr){
+            if (error || stderr) {
                 console.log(stderr);
-                return deferred.reject({"error": stderr});
+                return deferred.reject({ "error": stderr });
             }
-            return deferred.resolve(stdout? stdout: "Success");
+            return deferred.resolve(stdout ? stdout : "Success");
         }
     );
     return deferred.promise;
 }
 
 
-function deleteDirectory(action){
-    var deferred = q.defer();
-    var execString = action.method.actionString;
-    for (var i =0; i< action.method.params.length;i++){
-        var param = action.method.params[i].name;
+function deleteDirectory(action) {
+    let deferred = q.defer();
+    let execString = action.method.actionString;
+    for (let i = 0; i < action.method.params.length; i++) {
+        let param = action.method.params[i].name;
         if (action.params.hasOwnProperty(param)) {
             execString = execString.replace(param, action.params[param]);
         }
-        else{
+        else {
             execString = execString.replace(param, '');
         }
     }
     // check if os is windows
-    execString = /^win/.test(process.platform)? "rmdir " + execString: "rm " + execString;
+    execString = /^win/.test(process.platform) ? "rmdir " + execString : "rm " + execString;
     exec(execString,
-        function(error, stdout, stderr){
+        function (error, stdout, stderr) {
             console.log(stdout);
-            if(error || stderr){
+            if (error || stderr) {
                 console.log(stderr);
-                return deferred.reject({"error": stderr});
+                return deferred.reject({ "error": stderr });
             }
-            return deferred.resolve(stdout? stdout: "Deleted directory");
+            return deferred.resolve(stdout ? stdout : "Deleted directory");
         }
     );
     return deferred.promise;
 }
 
+function pathExsits(action) {
+    return new Promise((resolve, reject) => {
+        let path = action.params.PATH;
+        fs.exists(path, function (exists) {
+            return resolve({ "res": exists })
+        });
+    });
+}
 
-var functions = {
+
+let functions = {
     copyPath: executeCMD,
     createDirectory: executeCMD,
     moveDirectory: executeCMD,
-    deleteDirectory : deleteDirectory
-}
+    deleteDirectory: deleteDirectory,
+    exists: pathExsits
+};
 
 
 function main(argv) {
@@ -71,11 +82,11 @@ function main(argv) {
         // Either an unknown option was specified, or an option requiring a value was provided without a value.
         process.exit(9);
     }
-    var action = JSON.parse(argv[2]);
-    functions[action.method.name](action).then(function(res) {
+    let action = JSON.parse(argv[2]);
+    functions[action.method.name](action).then(function (res) {
         console.log(res);
         process.exit(0); // Success
-    }, function(err) {
+    }, function (err) {
         console.log("an error occured", err);
         // Uncaught Fatal Exception
         // There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
