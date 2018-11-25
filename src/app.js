cf2e8befb9faca3cfe41dc00ec9711a9ac10b581
kadosh1000 <kadosh10001@gmail.com>
2018-11-25 12:28:38 +0200

    * fix plugin structure

--- src/app.js
+++ src/app.js
@@ -66,32 +66,10 @@ function pathExsits(action) {
 }
 
 
-let functions = {
+module.exports = {
     copyPath: executeCMD,
     createDirectory: executeCMD,
     moveDirectory: executeCMD,
     deleteDirectory: deleteDirectory,
     exists: pathExsits
 };
-
-
-function main(argv) {
-    if (argv.length < 3) {
-        console.log('{err: "not enough parameters"}');
-        // Invalid Argument
-        // Either an unknown option was specified, or an option requiring a value was provided without a value.
-        process.exit(9);
-    }
-    let action = JSON.parse(argv[2]);
-    functions[action.method.name](action).then(function (res) {
-        console.log(res);
-        process.exit(0); // Success
-    }, function (err) {
-        console.log("an error occured", err);
-        // Uncaught Fatal Exception
-        // There was an uncaught exception, and it was not handled by a domain or an 'uncaughtException' event handler.
-        process.exit(1); // Failure
-    });
-}
-
-main(process.argv);
